19d89c2182573155cfb1ee3141d438c0ac8cdb0a
Elise Walker <elise@Elises-MacBook.local>
2016-08-11 09:07:14 -0400

    Binary update

--- goserver.go
+++ goserver.go
@@ -8,12 +8,14 @@ import (
 )
 
 func main() {
-    fmt.Println("hello world")
+    fmt.Println("Initializing Hello World Server...")
 
+    // Retrieving url path and printing it after hello
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
     })
 
+    // Serving at port 8080
     log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
